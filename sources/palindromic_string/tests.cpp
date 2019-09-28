9bbd7896eef0dfd7ff4261f595712cee59dbd1d2
Jacek Jankowski <jacek143@gmail.com>
2019-09-28 23:09:35 +0200

    Remove lint warnings

--- sources/palindromic_string/tests.cpp
+++ sources/palindromic_string/tests.cpp
@@ -1,14 +1,8 @@
 #include "gtest/gtest.h"
 #include "palindromic_string.h"
 
-using std::string;
 using palindromic_string::isPalindrome;
-
-TEST(PalindromicString, signature)
-{
-    string input = "";
-    bool answer = isPalindrome(input);
-}
+using std::string;
 
 TEST(PalindromicString, simpleYes)
 {
