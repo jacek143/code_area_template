f9498a3240ae5e875ebb967277126cf4352d332e
Jacek Jankowski <jacek143@gmail.com>
2019-10-02 19:22:39 +0200

    Treat lint warnings as errors

--- lint.sh
+++ lint.sh
@@ -5,5 +5,5 @@ do
     if [[ ${FILE_TO_CHECK} == *"/tests.cpp" ]]; then
         MODIF=''',-cppcoreguidelines-owning-memory,-cppcoreguidelines-special-member-functions'''
     fi
-    clang-tidy -p build -checks=cppcoreguidelines-*${MODIF} ${FILE_TO_CHECK}
+    clang-tidy -p build --quiet --warnings-as-errors=* -checks=cppcoreguidelines-*${MODIF} ${FILE_TO_CHECK}
 done
