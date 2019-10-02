c9c32461251a3a1aadd63ab69ee97c4d4dd98104
Jacek Jankowski <jacek143@gmail.com>
2019-10-02 19:39:27 +0200

    Exit lint script with error code when something is detected

--- lint.sh
+++ lint.sh
@@ -1,9 +1,20 @@
 ./cmake.sh
 
+IS_ANY_ERROR=false
 for FILE_TO_CHECK in $(find sources | grep '.cpp')
 do
     if [[ ${FILE_TO_CHECK} == *"/tests.cpp" ]]; then
         MODIF=''',-cppcoreguidelines-owning-memory,-cppcoreguidelines-special-member-functions'''
     fi
-    clang-tidy -p build --quiet --warnings-as-errors=* -checks=cppcoreguidelines-*${MODIF} ${FILE_TO_CHECK}
+    ERROR_LOG=$(clang-tidy -p build --quiet --warnings-as-errors=* -checks=cppcoreguidelines-*${MODIF} ${FILE_TO_CHECK})
+    if !(test -z "${ERROR_LOG}")
+    then
+        IS_ANY_ERROR=true
+        printf '%s\n' "${ERROR_LOG[@]}"
+    fi
 done
+
+if (${IS_ANY_ERROR})
+then 
+    exit 1
+fi
