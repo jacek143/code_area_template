3cea90f7419bdebdb427be1d3cbb3ffcf6133ed7
Jacek Jankowski <jacek143@gmail.com>
2019-10-04 13:38:57 +0200

    Integrate lint into cmake

--- lint.sh
+++ lint.sh
@@ -1,12 +1,11 @@
-./cmake.sh
-
+#!/bin/bash
 IS_ANY_ERROR=false
-for FILE_TO_CHECK in $(find sources | grep '.cpp')
+for FILE_TO_CHECK in $@
 do
     if [[ ${FILE_TO_CHECK} == *"/tests.cpp" ]]; then
         MODIF=''',-cppcoreguidelines-owning-memory,-cppcoreguidelines-special-member-functions'''
     fi
-    ERROR_LOG=$(clang-tidy -p build --quiet --warnings-as-errors=* -checks=cppcoreguidelines-*${MODIF} ${FILE_TO_CHECK})
+    ERROR_LOG=$(clang-tidy -p build --quiet --warnings-as-errors=* -checks=cppcoreguidelines-*${MODIF} ${FILE_TO_CHECK} 2>/dev/null)
     if !(test -z "${ERROR_LOG}")
     then
         IS_ANY_ERROR=true
