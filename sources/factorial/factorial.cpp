94783a7b2e95a0c8ef6ccddb77f526d1a52716d9
Jacek Jankowski <jacek143@gmail.com>
2019-10-04 14:35:48 +0200

    Check if bad formatting breaks CI

--- sources/factorial/factorial.cpp
+++ sources/factorial/factorial.cpp
@@ -1,7 +1,7 @@
 #include "factorial.h"
 
 unsigned factorial::computeFactorial(unsigned n) {
-  unsigned result = 1;
+       unsigned result = 1;
   for (int i = 2; i <= n; i++) {
     result *= i;
   }
