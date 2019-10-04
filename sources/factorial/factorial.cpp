23acbeebe55d29e8626a14cfb4d862426d7ef74d
Jacek Jankowski <jacek143@gmail.com>
2019-10-04 19:11:21 +0200

    Add more compilation warnings

--- sources/factorial/factorial.cpp
+++ sources/factorial/factorial.cpp
@@ -2,7 +2,7 @@
 
 unsigned factorial::computeFactorial(unsigned n) {
   unsigned result = 1;
-  for (int i = 2; i <= n; i++) {
+  for (unsigned i = 2; i <= n; i++) {
     result *= i;
   }
   return result;
