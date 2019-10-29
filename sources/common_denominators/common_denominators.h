e984a0a0df22a390162268f76b33f546ede5a322
Jacek Jankowski <jacek143@gmail.com>
2019-10-29 23:24:38 +0100

    Solve "Common Denominator" problem

--- sources/common_denominators/common_denominators.h
+++ sources/common_denominators/common_denominators.h
@@ -1,14 +1,73 @@
 #ifndef COMMON_DENOMINATORS_H
 #define COMMON_DENOMINATORS_H
 
+#include <algorithm>
+#include <sstream>
 #include <string>
 #include <vector>
 
 namespace Fracts {
-std::string
-convertFrac(const std::vector<std::vector<unsigned long long>> &rationals) {
-  (void)rationals;
-  return "(1,2)";
+using Integer = unsigned long long;
+using Rational = std::vector<Integer>;
+using RationalsList = std::vector<Rational>;
+
+Integer smallestCommonMultiple(Integer a, Integer b) {
+  auto multiple = a * b;
+  for (Integer i = 2; i <= std::min(a, b); i++) {
+    while (a % i == 0 and b % i == 0) {
+      multiple /= i;
+      a /= i;
+      b /= i;
+    }
+  }
+  return multiple;
+}
+
+Integer smallestCommonDenominator(RationalsList rationals) {
+  Integer denominator = 1;
+  for (const auto &rational : rationals) {
+    denominator = smallestCommonMultiple(denominator, rational[1]);
+  }
+  return denominator;
+}
+
+std::string listToString(const RationalsList &list) {
+  std::stringstream stream;
+  for (auto &&rational : list) {
+    stream << "(" << rational.at(0) << "," << rational.at(1) << ")";
+  }
+  return stream.str();
+}
+
+Rational simplify(Rational number) {
+  for (Integer i = 2; i <= number[1]; i++) {
+    while (number[0] % i == 0 and number[1] % i == 0) {
+      number[0] /= i;
+      number[1] /= i;
+    }
+  }
+  return number;
+}
+
+RationalsList simplify(RationalsList rationals) {
+  RationalsList processed;
+  while (rationals.size() > 0) {
+    Rational number = simplify(rationals.back());
+    rationals.pop_back();
+    processed.insert(begin(processed), number);
+  }
+  return processed;
+}
+
+std::string convertFrac(RationalsList rationals) {
+  rationals = simplify(rationals);
+  auto denominator = smallestCommonDenominator(rationals);
+  for (auto &r : rationals) {
+    auto gain = denominator / r[1];
+    r[0] *= gain;
+    r[1] *= gain;
+  }
+  return listToString(rationals);
 }
 } // namespace Fracts
 
--- sources/common_denominators/tests.cpp
+++ sources/common_denominators/tests.cpp
@@ -2,11 +2,34 @@
 #include "gtest/gtest.h"
 
 using Fracts::convertFrac;
-using std::vector;
+using Fracts::smallestCommonDenominator;
+using Fracts::smallestCommonMultiple;
 
-using Rationals = vector<vector<unsigned long long>>;
+TEST(CommonDenominatorsTests, simplifyOneRational) {
+  EXPECT_EQ("(1,3)", convertFrac({{3, 9}}));
+  EXPECT_EQ("(1,2)", convertFrac({{6, 12}}));
+  EXPECT_EQ("(3,4)", convertFrac({{12, 16}}));
+  EXPECT_EQ("(2,1)", convertFrac({{34, 17}}));
+}
 
-TEST(CommonDenominatorsTests, onlyOneRational) {
-  const Rationals rationals = {{1, 2}};
-  EXPECT_EQ("(1,2)", convertFrac(rationals));
+TEST(CommonDenominatorsTests, twoRationals) {
+  EXPECT_EQ("(3,6)(2,6)", convertFrac({{1, 2}, {1, 3}}));
+}
+
+TEST(CommonDenominatorsTests, threeRationals) {
+  EXPECT_EQ("(6,12)(4,12)(3,12)", convertFrac({{1, 2}, {1, 3}, {1, 4}}));
+}
+
+TEST(CommonDenominatorsTests, smallestCommonMultiple) {
+  EXPECT_EQ(1, smallestCommonMultiple(1, 1));
+  EXPECT_EQ(6, smallestCommonMultiple(2, 3));
+  EXPECT_EQ(18, smallestCommonMultiple(6, 9));
+  EXPECT_EQ(6, smallestCommonMultiple(3, 6));
+  EXPECT_EQ(12, smallestCommonMultiple(12, 4));
+}
+
+TEST(CommonDenominatorsTests, smallestCommonDenominator) {
+  EXPECT_EQ(18, smallestCommonDenominator({{1, 6}, {1, 9}}));
+  EXPECT_EQ(6, smallestCommonDenominator({{1, 2}, {1, 3}}));
+  EXPECT_EQ(12, smallestCommonDenominator({{1, 2}, {1, 3}, {1, 4}}));
 }
