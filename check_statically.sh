ecd7d51f11843a4ede135360bd0c0ec542efd3aa
Jacek Jankowski <jacek143@gmail.com>
2019-11-03 22:57:36 +0100

    Change clang-tidy script to show reason of failure

--- check_statically.sh
+++ check_statically.sh
@@ -7,15 +7,5 @@ do
     if [[ ${FILE_TO_CHECK} == *"/tests.cpp" ]]; then
         MODIF=''',-cppcoreguidelines-owning-memory,-*-special-member-functions'''
     fi
-    ERROR_LOG=$(clang-tidy -p build --quiet --warnings-as-errors=* -checks=${COMMON_CHECKS}${MODIF} ${FILE_TO_CHECK} 2>/dev/null)
-    if !(test -z "${ERROR_LOG}")
-    then
-        IS_ANY_ERROR=true
-        printf '%s\n' "${ERROR_LOG[@]}"
-    fi
+    clang-tidy -p build --quiet --warnings-as-errors=* -checks=${COMMON_CHECKS}${MODIF} ${FILE_TO_CHECK}
 done
-
-if (${IS_ANY_ERROR})
-then 
-    exit 1
-fi
